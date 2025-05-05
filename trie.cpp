59b3b5eed69d410f8eb7ffedfe1fc04a1759437e
samyaksgupta <samyaksanjiv@gmail.com>
2025-05-05 23:26:49 +0530

    adding file trie.cpp to test

---
+++ trie.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class TrieNode {
+	public:
+	  char data;
+	  TrieNode* children[26];
+	  bool isTerm;
+
+	  TrieNode(char ch) {
+		  data = ch;
+		  for (int i = 0 ; i < 26 ; i++) {
+			  children[i] = NULL;
+		  }
+		  is terminal = false;
+	  }
+
+};
+class Trie {
+	public:
+	TrieNode* root;
+	
+	void insertword(string word) {
+	}
+
+
+
+
+
